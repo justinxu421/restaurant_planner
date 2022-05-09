0ffd5b261a4a405280cfd07d456f0f637e35f580
Justin Xu <jxu421@gmail.com>
2022-05-08 20:02:09 -0400

    update cors

--- fast_api_boba_app/app/main.py
+++ fast_api_boba_app/app/main.py
@@ -6,7 +6,7 @@ from app.endpoints import business
 
 app = FastAPI()
 
-origins = ["http://localhost:3000", "https://justinxu421.github.io"]
+origins = ["http://localhost:3000", "https://justinxu421.github.io/restaurant_planner"]
 
 app.add_middleware(
     CORSMiddleware,
